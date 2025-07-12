794d27d39572db13812a15fbd9dbf2b6ee4236c0
owncook1 <chopgaming8@gmail.com>
2025-07-12 11:50:29 +0530

    fixes

--- pepecoin-donation.js
+++ pepecoin-donation.js
@@ -2,12 +2,15 @@ class PepecoinDonation extends HTMLElement {
     connectedCallback() {
       const address = this.getAttribute('address') || 'YOUR_DEFAULT_PEPE_ADDRESS';
       const title = this.getAttribute('title') || 'Donate Pepecoin';
-      const color = this.getAttribute('color') || '#4CAF50';
+      const color = this.getAttribute('color') || '#181816';
   
       const shadow = this.attachShadow({ mode: 'open' });
       shadow.innerHTML = `
         <style>
+            @import url('https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap');
+
           .donation-box {
+            font-family: 'Nunito', sans-serif;
             display: flex;
             flex-direction: column;
             align-items: center;
@@ -24,21 +27,23 @@ class PepecoinDonation extends HTMLElement {
             color: ${color};
           }
           .donation-address {
+            font-family: 'Nunito', sans-serif;
             font-size: 0.8rem;
             word-break: break-all;
             text-align: center;
-            margin-bottom: 0.5rem;
-            color: #333;
+            color: ${color};
+            font-weight: bold;
           }
           .donation-qr {
             width: 128px;
             height: 128px;
+            margin-bottom: 0.8rem;
           }
         </style>
         <div class="donation-box">
           <div class="donation-title">${title}</div>
+          <img class="donation-qr" src="https://api.qrserver.com/v1/create-qr-code/?data=pepecoin:${address}&size=128x128" alt="QR Code">
           <div class="donation-address">${address}</div>
-          <img class="donation-qr" src="https://pepeblocks.com/qr/${address}" alt="Pepecoin QR code" />
         </div>
       `;
     }
